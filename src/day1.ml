bf7b5fa691268d584bc044f926c4e2d56a197651
ZixuImperial <zixuw1@gmail.com>
2026-01-09 16:15:19 +0000

    fix: left logic for day1 fixed so increments are correct for clock cycles.

--- src/day1.ml
+++ src/day1.ml
@@ -73,31 +73,51 @@ let create scope ({ clock; clear; start; finish; data_in; data_in_valid } : _ I.
                 [ if_
                     (msb data_in)
                     (* To avoid pain with dealing with signed input, I have converted it 
-                       to be unsigned while keeping the modulus working *)
-                    [ (let ptr' = hundred_sig -: ptr.value -: data_in in
+                       to be unsigned so I can correctly work out the quot and rem. 
+                       By doing (100 - ptr.value + abs data_in) % 100,
+                       I can do 100 - on the rem to find the correct position. *)
+                    [ ((* Find below explanation of fast quotRem. *)
+                       let ptr' = hundred_sig -: ptr.value -: data_in in
                        let prod = ptr' *: recip_sig in
-                       let q = sel_top prod ~width:num_bits in
-                       let r = ptr' -: uresize ~width:num_bits (q *: hundred_sig) in
-                       let value = hundred_sig -: r in 
+                       let quot = sel_top prod ~width:num_bits in
+                       let rem = ptr' -: uresize ~width:num_bits (quot *: hundred_sig) in
+                       let value = hundred_sig -: rem in
+                       (* Needed to evaluate edge cases as when value <> 0. *)
+                       let edge = value <>:. 0 in
+                       (* Not sure how to still use the lets in continuous statements 
+                          without using when_ vdd. *)
                        when_
                          vdd
                          [ ptr <-- value
-                         ; part2 <-- part2.value +: q
+                         ; (* There was a problem when doing a left on 0.
+                              It would increment part2 due to how my division requires 
+                              100 - ptr.value + data_in which makes quot larger
+                              but there was also an issue where when value was 0,
+                              part2 did not increment so created if to sort logic. *)
+                           if_
+                             (ptr.value ==:. 0 &: edge)
+                             [ part2 <-- part2.value +: quot -:. 1 ]
+                             [ if_
+                                 edge
+                                 [ part2 <-- part2.value +: quot ]
+                                 [ part2 <-- part2.value +: quot +:. 1 ]
+                             ]
                          ; when_ (value ==:. 0) [ part1 <-- part1.value +:. 1 ]
                          ])
                     ]
-                    [ (let ptr' = ptr.value +: data_in in
+                    [ ((* Find below explanation of fast quotRem. *)
+                      let ptr' = ptr.value +: data_in in
                        let prod = ptr' *: recip_sig in
-                       let q = sel_top prod ~width:num_bits in
-                       let r = ptr' -: uresize ~width:num_bits (q *: hundred_sig) in
+                       let quot = sel_top prod ~width:num_bits in
+                       let rem = ptr' -: uresize ~width:num_bits (quot *: hundred_sig) in
                        (* Seems to be an edge case where the division will allow r to be 100. *)
                        if_
-                         (r ==:. 100)
+                         (rem ==:. 100)
                          [ ptr <-- zero num_bits
-                         ; part2 <-- part2.value +: q +:. 1
+                         ; part2 <-- part2.value +: quot +:. 1
                          ; part1 <-- part1.value +:. 1
                          ]
-                         [ ptr <-- r; part2 <-- part2.value +: q ])
+                         [ ptr <-- rem; part2 <-- part2.value +: quot ])
                     ]
                 ]
             ; when_ finish [ sm.set_next Done ]
@@ -112,3 +132,14 @@ let hierarchical scope =
   let module Scoped = Hierarchy.In_scope (I) (O) in
   Scoped.hierarchical ~scope ~name:"day1" create
 ;;
+
+(* Motivation for quotRem. 
+   We can only use this method, if we have constraints and knowledge of which divisor
+   we want. Let x be our input, d be our divisor, n be size, q be quot, and r be rem. Then
+   q = x / d. Then we can multiply by 2^n / 2^n so we get q = (x * 2^n) / (d * 2^n).
+   Same as q = (x / 2^n) * (2^n / d) so as we know d and n. We can precompute 2^n / d
+   so as dividing by a power of 2 is the same as right shifting we can know q to be
+   approximately (x * (2^n / d)) >> n which can be done in 1 cycle as we know
+   (2^n / d) as recip_multiplier. Then we can easily work out r by x - (q * d) 
+   as that is what the remainder is. Instead of just shifting I can use sel_top
+   n bits as the multiplication has 32 bits so right shifting is the same. *)
