0fc5ec4a9a4405ee9c6c365fa47055e474f8239c
ZixuImperial <zixuw1@gmail.com>
2026-01-15 00:40:51 +0000

    chore: added day4 to the generate for Verilog source

--- bin/generate.ml
+++ bin/generate.ml
@@ -21,6 +21,25 @@ let day1_rtl_command =
       fun () -> generate_day1_rtl ()]
 ;;
 
+let generate_day4_rtl () =
+  let module C = Circuit.With_interface (Day4.I) (Day4.O) in
+  let scope = Scope.create ~auto_label_hierarchical_ports:true () in
+  let circuit = C.create_exn ~name:"day1_top" (Day4.hierarchical scope) in
+  let rtl_circuits =
+    Rtl.create ~database:(Scope.circuit_database scope) Verilog [ circuit ]
+  in
+  let rtl = Rtl.full_hierarchy rtl_circuits |> Rope.to_string in
+  print_endline rtl
+;;
+
+let day4_rtl_command =
+  Command.basic
+    ~summary:""
+    [%map_open.Command
+      let () = return () in
+      fun () -> generate_day4_rtl ()]
+;;
+
 let generate_range_finder_rtl () =
   let module C = Circuit.With_interface (Range_finder.I) (Range_finder.O) in
   let scope = Scope.create ~auto_label_hierarchical_ports:true () in
@@ -42,5 +61,5 @@ let range_finder_rtl_command =
 
 let () =
   Command_unix.run
-    (Command.group ~summary:"" [ ("range-finder", range_finder_rtl_command) ; ("day1", day1_rtl_command)])
+    (Command.group ~summary:"" [ ("range-finder", range_finder_rtl_command) ; ("day1", day1_rtl_command); ("day4", day4_rtl_command)])
 ;;
